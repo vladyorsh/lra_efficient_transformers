79f71c7cc33f45e9e06600bc06ca7ee20f44dbd8
Uladzislau Yorsh <vladyorsh@gmail.com>
2023-07-14 20:21:34 +0300

    migrating to lightning

--- lra_run.py
+++ lra_run.py
@@ -71,7 +71,6 @@ def get_model(task, length, setup, model, encoder, log_non_scalars, logging_freq
             output_dropout_rate=setup['output_dropout_rate'],
             affine=setup['affine'],
             logging_frequency=logging_frequency,
-            log_params=log_params,
         ),
         reg_weight=1.0,
         betas=(0.9, 0.999), #Original LRA uses 0.98, but may yield quite unsatisfying results
@@ -79,6 +78,7 @@ def get_model(task, length, setup, model, encoder, log_non_scalars, logging_freq
         wd=setup['weight_decay'],
         schedule=setup['schedule'](),
         log_non_scalars=log_non_scalars,
+        log_params=log_params,
     )
     
     return model
