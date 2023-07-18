c57cbd823df05b4167b9bf2931ebecff16409a02
Uladzislau Yorsh <vladyorsh@gmail.com>
2023-07-18 23:34:13 +0300

    improved logging

--- lra/models.py
+++ lra/models.py
@@ -119,7 +119,7 @@ class PreLunaClassifier(LunaClassifier):
     self.encoder     = Encoder(PreLunaBlock, num_blocks, hidden_dim, qkv_dim, mlp_dim, num_heads, internal_dropout_rate, affine, logging_frequency, shared_att)
 
 class PreLunaMatcher(LunaMatcher):
-    def __init__(self, classes, num_embeddings, seq_len, hidden_dim, qkv_dim, mlp_dim, num_heads, num_blocks, internal_dropout_rate=0.1, output_dropout_rate=0.0, affine=True, logging_frequency=1000, mem_size=256):
+    def __init__(self, classes, num_embeddings, seq_len, hidden_dim, qkv_dim, mlp_dim, num_heads, num_blocks, internal_dropout_rate=0.1, output_dropout_rate=0.0, affine=True, logging_frequency=1000, mem_size=256, shared_att='full'):
         super(PreLunaMatcher, self).__init__(classes, num_embeddings, seq_len, hidden_dim, qkv_dim, mlp_dim, num_heads, num_blocks, internal_dropout_rate, output_dropout_rate, affine, logging_frequency, mem_size, shared_att)
         self.encoder     = Encoder(PreLunaBlock, num_blocks, hidden_dim, qkv_dim, mlp_dim, num_heads, internal_dropout_rate, affine, logging_frequency, shared_att)
 
