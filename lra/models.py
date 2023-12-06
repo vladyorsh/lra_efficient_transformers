8661f9f9eee8ea0cf9f3e91f6f97fd21d8025bc4
Uladzislau Yorsh <vladyorsh@gmail.com>
2023-12-07 00:19:39 +0100

    added simplified convluna matcher

--- lra/models.py
+++ lra/models.py
@@ -155,8 +155,8 @@ class LunaMatcher(LunaClassifier):
     
 class SimplifiedConvLunaMatcher(SimplifiedConvLunaClassifier):
   def __init__(self, classes, num_embeddings, seq_len, hidden_dim, qkv_dim, mlp_dim, num_heads, num_blocks, internal_dropout_rate=0.1, output_dropout_rate=0.0, affine=True, use_cls=True, logging_frequency=1000, norm_type='layernorm', mem_size=256, kernel=(4, 1), stride=(1, 1), pool=False, temperature_pack='unit', temperature_unpack='unit', use_mem_repr=False):
-    super(SimplifiedConvLunaClassifier, self).__init__(classes, num_embeddings, seq_len, hidden_dim, qkv_dim, mlp_dim, num_heads, num_blocks, internal_dropout_rate, output_dropout_rate, affine, use_cls, logging_frequency, norm_type, mem_size, kernel, stride, pool, temperature_pack, temperature_unpack, use_mem_repr)
-     self.classifier  = DualClassifier(classes, hidden_dim, mlp_dim, affine, use_cls)
+    super(SimplifiedConvLunaMatcher, self).__init__(classes, num_embeddings, seq_len, hidden_dim, qkv_dim, mlp_dim, num_heads, num_blocks, internal_dropout_rate, output_dropout_rate, affine, use_cls, logging_frequency, norm_type, mem_size, kernel, stride, pool, temperature_pack, temperature_unpack, use_mem_repr)
+    self.classifier  = DualClassifier(classes, hidden_dim, mlp_dim, affine, use_cls)
 
   def forward(self, inputs, masks):
     additional_losses = []
--- lra/setups.py
+++ lra/setups.py
@@ -28,19 +28,19 @@ LISTOPS_SETUP = {
     'full_batch_size' : 64, #Original 32
     'max_length' : 2000,
     
-    'lr' : 0.01, #Original 0.005
+    'lr' : 0.005, #Original 0.005
     'weight_decay' : 0.01, #Original 0.1
     'schedule' : lambda: get_sqrt_schedule(warmup_steps=1000),
-    'steps' : 20000,
+    'steps' : 30000,
     'eval_period' : 200, #Original 50
     
     'classes' : 10,
-    'hidden_dim' : 80, #Original 512
-    'qkv_dim' : 80, #Original 512
-    'num_heads' : 8,
+    'hidden_dim' : 128, #Original 512, try 256
+    'qkv_dim' : 128, #Original 512, try 256
+    'num_heads' : 4, #Original 8
     'num_blocks' : 6,
-    'mlp_dim' : 320, #Original 2048
-    'output_units' : 320, #Original 2048
+    'mlp_dim' : 512, #Original 2048
+    'output_units' : 512, #Original 2048
     'internal_dropout_rate' : 0.1,
     'output_dropout_rate' : 0.0,
     
