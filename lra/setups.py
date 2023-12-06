9670b87992411ac4598468427ad00616e5b168c3
Uladzislau Yorsh <vladyorsh@gmail.com>
2023-12-06 19:58:59 +0100

    other task setups

--- lra/setups.py
+++ lra/setups.py
@@ -25,22 +25,22 @@ CLS_SETUP = {
 }
 
 LISTOPS_SETUP = {
-    'full_batch_size' : 32,
+    'full_batch_size' : 64, #Original 32
     'max_length' : 2000,
     
-    'lr' : 0.005,
-    'weight_decay' : 0.1,
+    'lr' : 0.005, #try 0.001?
+    'weight_decay' : 0.01, #Original 0.1
     'schedule' : lambda: get_sqrt_schedule(warmup_steps=1000),
     'steps' : 15000,
-    'eval_period' : 50,
+    'eval_period' : 200, #Original 50
     
     'classes' : 10,
-    'hidden_dim' : 512,
-    'qkv_dim' : 512,
+    'hidden_dim' : 80, #Original 512
+    'qkv_dim' : 80, #Original 512
     'num_heads' : 8,
     'num_blocks' : 6,
-    'mlp_dim' : 2048,
-    'output_units' : 2048,
+    'mlp_dim' : 320, #Original 2048
+    'output_units' : 320, #Original 2048
     'internal_dropout_rate' : 0.1,
     'output_dropout_rate' : 0.0,
     
@@ -49,14 +49,14 @@ LISTOPS_SETUP = {
 }
 
 MATCHING_SETUP = {
-    'full_batch_size' : 32,
+    'full_batch_size' : 64, #Original 32
     'max_length' : 4000,
     
     'lr' : 0.05,
-    'weight_decay' : 0.1,
+    'weight_decay' : 0.04, #Original 0.01
     'schedule' : lambda: get_sqrt_schedule(warmup_steps=8000),
     'steps' : 30000,
-    'eval_period' : 200,
+    'eval_period' : 500, #Original 200
     'skip_eval' : 0,
     
     'classes' : 2,
@@ -64,7 +64,7 @@ MATCHING_SETUP = {
     'qkv_dim' : 128,
     'mlp_dim' : 512,
     'num_heads' : 4,
-    'num_blocks' : 4,
+    'num_blocks' : 6, #Original 4
     'output_units' : 512,
     'internal_dropout_rate' : 0.1,
     'output_dropout_rate' : 0.0,
