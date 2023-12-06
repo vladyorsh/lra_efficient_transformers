a1ec9d368797fcb54397d5b2cb1fa8ded24276b7
Uladzislau Yorsh <vladyorsh@gmail.com>
2023-12-06 21:24:23 +0100

    updated other serups

--- lra/setups.py
+++ lra/setups.py
@@ -28,10 +28,10 @@ LISTOPS_SETUP = {
     'full_batch_size' : 64, #Original 32
     'max_length' : 2000,
     
-    'lr' : 0.005, #try 0.001?
+    'lr' : 0.01, #Original 0.005
     'weight_decay' : 0.01, #Original 0.1
     'schedule' : lambda: get_sqrt_schedule(warmup_steps=1000),
-    'steps' : 15000,
+    'steps' : 20000,
     'eval_period' : 200, #Original 50
     
     'classes' : 10,
