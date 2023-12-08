5baa7d5b50d4e20401d999dade3b0df9d22ab736
Uladzislau Yorsh <vladyorsh@gmail.com>
2023-12-08 19:37:13 +0100

    image models

--- lra_run.py
+++ lra_run.py
@@ -34,7 +34,7 @@ def get_lra_data(lib_path, data_path, task, batch_size, max_length):
         'matching'       : lambda batch_size, max_length: get_matching_datasets(1, None, tokenizer='char', data_dir=MATCHING_DATADIR, batch_size=batch_size, max_length=max_length),
         'listops'        : lambda batch_size, max_length: get_listops_datasets(1, 'basic', data_dir=LISTOPS_DATADIR, batch_size=batch_size, max_length=max_length),
         'cifar'          : lambda batch_size, max_length: get_cifar10_datasets(1, batch_size, normalize=False)[:3] + (None,),
-        'pathfinder'     : lambda batch_size, max_length: get_pathfinder_base_datasets(1, batch_size, resolution=32, split='hard', normalize=False[:3]) + (None,)
+        'pathfinder'     : lambda batch_size, max_length: get_pathfinder_base_datasets(1, batch_size, resolution=32, split='hard', normalize=False)[:3] + (None,)
     }
     
     return DATASETS_BY_TASK[task.lower()](batch_size, max_length)
